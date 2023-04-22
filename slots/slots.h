e18e6ed0f25628190f39d25753a6454873a42374
pechansky <pechansky@hypervisual.com.br>
2023-04-22 17:20:40 -0300

    Prior to changing to Mega 2560

--- slots/slots.h
+++ slots/slots.h
@@ -10,35 +10,38 @@
 
 #include <Arduino.h>
 
+#define NREELS	3
+
 // Arduino pins
 
 const uint8_t leverButton = 8;
 const uint8_t coinIncrementButton = 12;
-
-const uint8_t encoder1 = 2;
-const uint8_t encoder2 = 4;
-const uint8_t encoder3 = 7;
-
+const uint8_t encoder[] = { 2, 4, 7 };
 const uint8_t motor1Ports[] = {3, 5};
 const uint8_t motor2Ports[] = {6, 9};
 const uint8_t motor3Ports[] = {10, 11};
-
-const uint8_t posSensor1 = A0;
-const uint8_t posSensor2 = A1;
-const uint8_t posSensor3 = A2;
-
+const uint8_t posSensor[] = { A0, A1, A2 };
 const uint8_t jackpotLED = 13;
-const uint8_t lockLED1 = A3;
-const uint8_t lockLED2 = A6;
-const uint8_t lockLED3 = A7;
+const uint8_t lockLED[] = { A3, A6, A7 };
 
 // Other constants
 
-const uint16_t initialPause = 1000;			// Initial pause before reducing speed
-const uint16_t reduceStep = 30;				// Steps between each reduction
-const uint16_t finalSteps = 260;			// Steps after sensor is triggered
-const uint8_t maxSpeed = 180;				// Maximum speed
-const uint8_t minSpeed = 120;				// Minimum speed
 const unsigned long debouncePeriod = 1000;	// Microseconds
+
+// const uint16_t initialSteps = 260;			// Steps after sensor is triggered
+// const uint16_t speedDecrement = 5;			// Speed decrement between each reduction
+// const uint16_t finalSteps = 30;				// Steps between each reduction
+// const uint8_t maxSpeed = 180;				// Maximum speed
+// const uint8_t normalSpeed = 100;			// Normal speed
+// const uint8_t minSpeed = 50;				// Minimum speed
+// const uint8_t extraRotations[] = {1, 8, 18};
+
+const uint16_t speedDecrement = 1;			// Speed decrement between each reduction
+// const uint16_t finalSteps[] = {100, 100, 100};				// Steps after sensor is triggered
+const uint16_t finalSteps[] = {0, 0, 0};				// Steps after sensor is triggered
+// const uint8_t maxSpeed = 80;				// Maximum speed
+const uint8_t normalSpeed[] = {80, 80, 80};			// Normal speed
+const uint8_t minSpeed[] = {50, 50, 50};				// Minimum speed
+const uint8_t extraRotations[] = {0, 0, 0};
 
 #endif	// SLOTS_H
--- slots/slots.ino
+++ slots/slots.ino
@@ -1,12 +1,14 @@
 
 #include "MotorDriver.h"
 #include "slots.h"
+#include <Wire.h>
+// #include <LiquidCrystal_I2C.h>
 
 // ----------------------------------------------------------------------- Types
 
 enum {
 	START = 0,
-	MEASURING,
+	SENSING,
 	COUNTING,
 	STOP,
 	WAITING,
@@ -18,25 +20,20 @@ enum {
 
 // ------------------------------------------------------------------- Variables
 
-MotorDriver motor1 = MotorDriver(motor1Ports, encoder1);
-MotorDriver motor2 = MotorDriver(motor2Ports, encoder2);
-MotorDriver motor3 = MotorDriver(motor3Ports, encoder3);
+MotorDriver motor[] = {
+	MotorDriver(motor1Ports, encoder[0]),
+	MotorDriver(motor2Ports, encoder[1]),
+	MotorDriver(motor3Ports, encoder[2])
+};
 
-uint16_t counter1;
-uint16_t counter2;
-uint16_t counter3;
+uint16_t counter[NREELS];
+byte currentSignal[NREELS];
+unsigned long lastChange[] = {0, 0, 0};
+byte state[] = { START, START, START };
+uint8_t speed[] = {normalSpeed[0], normalSpeed[1], normalSpeed[2]};
+uint8_t rotations[NREELS];// = {0, 0, 0};
 
-byte state1 = START;
-byte state2 = START;
-byte state3 = START;
-
-unsigned long lastChange1 = 0;
-unsigned long lastChange2 = 0;
-unsigned long lastChange3 = 0;
-
-byte currentSignal1;
-byte currentSignal2;
-byte currentSignal3;
+// LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // ----------------------------------------------------------------------- Setup
 
@@ -49,176 +46,115 @@ void setup()
 	pinMode(leverButton, INPUT_PULLUP);
 	pinMode(coinIncrementButton, INPUT_PULLUP);
 
-	pinMode(posSensor1, INPUT_PULLUP);
-	pinMode(posSensor2, INPUT_PULLUP);
-	pinMode(posSensor3, INPUT_PULLUP);
+	// lcd.init();
+	// lcd.backlight();      // Make sure backlight is on
+	// lcd.clear();
+	// lcd.setCursor(0, 0);
+	// lcd.print("Slot machine");
+
+	for(int n = 0; n < NREELS; n++) {
+		rotations[n] = extraRotations[n];
+	}
 }
 
 // ------------------------------------------------------------------- Main loop
 
+void __loop()
+{
+	motor[0].RotateCCW(80);
+	Serial.println("Motor 1 started");
+	delay(1000);
+
+	motor[1].RotateCCW(80);
+	Serial.println("Motor 2 started");
+	delay(1000);
+
+	motor[2].RotateCCW(80);
+	Serial.println("Motor 3 started");
+	delay(1000);
+
+	motor[0].Brake();
+	Serial.println("Motor 1 stopped");
+	delay(1000);
+
+	motor[1].Brake();
+	Serial.println("Motor 2 stopped");
+	delay(1000);
+
+	motor[2].Brake();
+	Serial.println("Motor 3 stopped");
+	delay(1000);
+}
+
 void loop()
 {
-	loop1();
-	loop2();
-	loop3();
+	processReel(0);
+	processReel(1);
+	processReel(2);
 }
 
-void loop1()
+void processReel(int n)
 {
-	switch(state1) {
+	switch(state[n]) {
 		case START:
-			motor1.RotateCCW(minSpeed);
-			
-			while(digitalRead(posSensor1) == LOW) {
-				// Wait until reed switch is opened
+			rotations[n] = extraRotations[n];
+			speed[n] = normalSpeed[n];
+			motor[n].RotateCCW(speed[n]);
+			while(digitalRead(posSensor[n])) {
+				// Wait while sensor is blocked
 			}
-
-			state1 = MEASURING;
+			state[n] = SENSING;
 			break;
 
-		case MEASURING:
-			if(digitalRead(posSensor1) == 0) {
-				Serial.println("Marker 1 detected.");
-				counter1 = finalSteps;
-				currentSignal1 = digitalRead(encoder1);
-				state1 = COUNTING;
+		case SENSING:
+			if(digitalRead(posSensor[n])) {
+				counter[n] = finalSteps[n];
+				currentSignal[n] = digitalRead(encoder[n]);
+				if(rotations[n] > 0) {
+					rotations[n]--;
+					// while(digitalRead(posSensor[n])) {
+					// 	// Wait while sensor is blocked
+					// }
+				} else {
+					// speed[n] = max(speed[n] - speedDecrement, minSpeed[n]);
+					// speed[n] = minSpeed[n];
+					// motor[n].RotateCCW(speed[n]);
+					state[n] = COUNTING;
+				}
 			}
 			break;
 
 		case COUNTING:
-			if(((micros() - lastChange1) > debouncePeriod) &&
-				(digitalRead(encoder1) != currentSignal1)) {
-				lastChange1 = micros();
-				currentSignal1 = !currentSignal1;
-				if(currentSignal1) {	 // RISING flank
-					if(--counter1 == 0) {
-						state1 = STOP;
+			if(((micros() - lastChange[n]) > debouncePeriod) &&
+				(digitalRead(encoder[n]) != currentSignal[n])) {
+				lastChange[n] = micros();
+				currentSignal[n] = !currentSignal[n];
+				if(currentSignal[n]) {	// RISING flank
+					if(counter[n]-- == 0) {
+						state[n] = STOP;
 					}
 				}
 			}
 			break;
 
 		case STOP:
-			motor1.Brake();
-			Serial.println("Stopped 1.");
-			state1 = WAITING;
+			motor[n].Brake();
+			state[n] = WAITING;
 			break;
 
 		case WAITING:
-			if(state2 == WAITING && state3 == WAITING) {
-				while(digitalRead(leverButton)) {
+			if(state[0] == WAITING && state[1] == WAITING && state[2] == WAITING) {
+				while(digitalRead(leverButton) == HIGH) {
 					// Wait for pushbutton release
 				}
-				if(!digitalRead(leverButton)) {
-					state1 = START;
-					state2 = START;
-					state3 = START;
+				if(digitalRead(leverButton) == LOW) {
+					state[0] = state[1] = state[2] = START;
+					// lcd.setCursor(0, 0);
+					// lcd.print("----  ----  ----");
+					// lcd.setCursor(0, 1);
+					// lcd.print("                ");
 				}
 			}
-			break;
-	}
-}
-
-void loop2()
-{
-	switch(state2) {
-		case START:
-			motor2.RotateCCW(minSpeed);
-			
-			while(digitalRead(posSensor2) == LOW) {
-				// Wait until reed switch is opened
-			}
-
-			state2 = MEASURING;
-			break;
-
-		case MEASURING:
-			if(digitalRead(posSensor2) == 0) {
-				Serial.println("Marker 2 detected.");
-				counter2 = finalSteps;
-				currentSignal2 = digitalRead(encoder2);
-				state2 = COUNTING;
-			}
-			break;
-
-		case COUNTING:
-			if(((micros() - lastChange2) > debouncePeriod) &&
-				(digitalRead(encoder2) != currentSignal2)) {
-				lastChange2 = micros();
-				currentSignal2 = !currentSignal2;
-				if(currentSignal2) {	 // RISING flank
-					if(--counter2 == 0) {
-						state2 = STOP;
-					}
-				}
-			}
-			break;
-
-		case STOP:
-			motor2.Brake();
-			Serial.println("Stopped 2.");
-			state2 = WAITING;
-			break;
-
-		case WAITING:
-			// while(digitalRead(leverButton)) {
-			// 	// Wait for pushbutton release
-			// }
-			// if(!digitalRead(leverButton)) {
-			// 	state2 = START;
-			// }
-			break;
-	}
-}
-
-void loop3()
-{
-	switch(state3) {
-		case START:
-			motor3.RotateCCW(minSpeed);
-			
-			while(digitalRead(posSensor3) == LOW) {
-				// Wait until reed switch is opened
-			}
-
-			state3 = MEASURING;
-			break;
-
-		case MEASURING:
-			if(digitalRead(posSensor3) == 0) {
-				Serial.println("Marker 3 detected.");
-				counter3 = finalSteps;
-				currentSignal3 = digitalRead(encoder3);
-				state3 = COUNTING;
-			}
-			break;
-
-		case COUNTING:
-			if(((micros() - lastChange3) > debouncePeriod) &&
-				(digitalRead(encoder3) != currentSignal3)) {
-				lastChange3 = micros();
-				currentSignal3 = !currentSignal3;
-				if(currentSignal3) {	 // RISING flank
-					if(--counter3 == 0) {
-						state3 = STOP;
-					}
-				}
-			}
-			break;
-
-		case STOP:
-			motor3.Brake();
-			Serial.println("Stopped 3.");
-			state3 = WAITING;
-			break;
-
-		case WAITING:
-			// while(digitalRead(leverButton)) {
-			// 	// Wait for pushbutton release
-			// }
-			// if(!digitalRead(leverButton)) {
-			// 	state3 = START;
-			// }
 			break;
 	}
 }
